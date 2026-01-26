6bc9f37b0fa3baa11d410dd63e7d1d3423ab78c6
Samadrita Dutta Gupta <samadrita.25bcs10173@sst.scaler.com>
2026-01-26 16:10:07 +0530

    Add files via upload

---
+++ expense.js
@@ -0,0 +1,151 @@
+const storedName=localStorage.getItem("currentUser");
+const userList=JSON.parse(localStorage.getItem("users"));
+const user=userList.find(u=>u.username===storedName);
+const allExpenses=user.expenses;
+console.log(allExpenses);
+const allExpenseDiv=document.querySelector(".all-expenses");
+if(allExpenses.length===0){
+    const noExpense=document.createElement("p");
+    noExpense.classList.add("no-expense");
+    noExpense.innerText="-No expenses added till now-"
+    allExpenseDiv.appendChild(noExpense);
+}
+[...allExpenses].reverse().forEach(function(expense){
+    const currentExpense=document.createElement("div");
+    currentExpense.dataset.category = expense.categories? expense.categories.toLowerCase(): "none";
+
+    currentExpense.classList.add("expense");
+    const type=expense.type;
+    if(type==="got"||type==="got_extra"){
+        currentExpense.dataset.color="green";
+    }
+    else if(type=="gave"){
+        currentExpense.dataset.color="red";
+    }
+    currentExpense.dataset.id = expense.id;
+    currentExpense.dataset.date = new Date(expense.date).toLocaleDateString("en-CA");
+
+    currentExpense.appendChild(document.createElement("header"));
+    const dateTime=document.createElement("h5");
+    dateTime.id="date_time";
+    dateTime.textContent = new Date(expense.date).toLocaleString();
+    currentExpense.appendChild(dateTime);
+    const section=document.createElement("section");
+    section.classList.add("expense_details");
+    const divInsideSection=document.createElement("div");
+    divInsideSection.classList.add("expense_details_name");
+    const pInsideDiv=document.createElement("p");
+    pInsideDiv.innerText=expense.descriptions;
+    const spanInsideDiv=document.createElement("span");
+    spanInsideDiv.innerText=expense.categories?`(${expense.categories})`:"";
+    divInsideSection.appendChild(pInsideDiv);
+    divInsideSection.appendChild(spanInsideDiv);
+    const amount=document.createElement("h6");
+    amount.innerText="₹"+expense.amounts;
+    section.appendChild(divInsideSection);
+    section.appendChild(amount);
+    currentExpense.appendChild(section);
+    const deletebutton=document.createElement("i");
+    deletebutton.classList.add("fa-solid");
+    deletebutton.classList.add("fa-trash");
+    deletebutton.classList.add("delete");
+    currentExpense.appendChild(deletebutton);
+    allExpenseDiv.appendChild(currentExpense);
+})
+//delete
+const deleteButtons=document.querySelectorAll(".delete");
+const modal=document.querySelector(".delete_modal")
+deleteButtons.forEach(function(delbtn){
+    delbtn.addEventListener("click",function(){
+        modal.style.display="flex";
+        const expenseDiv=delbtn.parentElement;
+        window.expenseToDelete = expenseDiv.dataset.id;
+
+    })
+})
+const deleteYes=document.querySelector("#delete");
+const deleteNo=document.querySelector("#dont-delete");
+deleteNo.addEventListener("click",function(){
+    modal.style.display="none";
+})
+deleteYes.addEventListener("click", function () {
+    const index = user.expenses.findIndex(
+        exp => exp.id == window.expenseToDelete
+    );
+    if (index === -1) {
+        modal.style.display = "none";
+        return;
+    }
+    const deletedExpense = user.expenses[index];
+    if (deletedExpense.type === "gave") {
+        user.balance += deletedExpense.amounts;
+    }
+    if (deletedExpense.type === "got" || deletedExpense.type === "got_extra") {
+        user.balance -= deletedExpense.amounts;
+    }
+    user.expenses.splice(index, 1);
+    localStorage.setItem("users", JSON.stringify(userList));
+    modal.style.display = "none";
+    location.reload();
+});
+
+
+const backToDashboard=document.querySelector(".dashboard");
+backToDashboard.addEventListener("click",function(){
+    window.location.href="index.html";
+})
+
+//SEARCH BAR
+const searchBar=document.querySelector("#search_expense");
+searchBar.addEventListener("input", function () {
+    const text = searchBar.value.toLowerCase();
+    const expenseDivs = document.querySelectorAll(".expense");
+
+    expenseDivs.forEach(function (div) {
+        const description = div.querySelector(".expense_details_name p").innerText.toLowerCase();
+
+        div.style.display = description.includes(text) ? "flex" : "none";
+    });
+});
+
+
+//FILTER
+const filterSelect = document.querySelector(".all-filters");
+
+filterSelect.addEventListener("change", function () {
+    const selectedCategory = filterSelect.value.toLowerCase();
+    const allExpenseDivs = document.querySelectorAll(".expense");
+    allExpenseDivs.forEach(div => {
+        div.style.display = "flex";
+    });
+    if (selectedCategory === "all") return;
+    allExpenseDivs.forEach(expenseDiv => {
+        if (expenseDiv.dataset.category !== selectedCategory) {
+            expenseDiv.style.display = "none";
+        }
+    });
+});
+
+
+const selectCalendar=document.querySelector(".calendar");
+const org_calendar=document.querySelector("#search_date");
+selectCalendar.addEventListener("click",function(){
+    org_calendar.showPicker();
+    org_calendar.addEventListener("change", function () {
+        const selectedDate = org_calendar.value;
+        const expenseDivs = document.querySelectorAll(".expense");
+
+        expenseDivs.forEach(div => {
+            const expenseDate = div.dataset.date;
+
+            if (expenseDate === selectedDate) {
+                div.style.display = "flex";
+            } else {
+                div.style.display = "none";
+            }
+        });
+    });
+
+
+})
+
---
+++ savings.js
@@ -0,0 +1,81 @@
+const backToDashboard=document.querySelector(".dashboard");
+backToDashboard.addEventListener("click",function(){
+    window.location.href="index.html";
+})
+const storedName=localStorage.getItem("currentUser");
+const userList=JSON.parse(localStorage.getItem("users"));
+const user=userList.find(u=>u.username===storedName);
+const allSavings = user.savingsHistory || [];
+
+console.log(allSavings);
+const allSavingDiv=document.querySelector(".all-savings");
+if(allSavings.length===0){
+    const noSavings=document.createElement("p");
+    noSavings.classList.add("no-savings");
+    noSavings.innerText="-No savings till now-";
+    allSavingDiv.appendChild(noSavings);
+}
+[...allSavings].reverse().forEach(function(saving,index){
+    const currentSaving=document.createElement("div");
+    currentSaving.classList.add("savings");
+    currentSaving.dataset.id = saving.id;
+    const dateTime=document.createElement("h5");
+    dateTime.id="date_time";
+    dateTime.textContent = new Date(saving.date).toLocaleString();
+    currentSaving.appendChild(dateTime);
+    const section=document.createElement("section");
+    section.classList.add("savings_details");
+    const pInsideSection=document.createElement("p");
+    pInsideSection.classList.add("savings_details_name");
+    pInsideSection.innerText=saving.description;
+    const amount=document.createElement("h6");
+    amount.innerText="₹"+saving.amount;
+    section.appendChild(pInsideSection);
+    section.appendChild(amount);
+    currentSaving.appendChild(section);
+    const deletebutton=document.createElement("i");
+    deletebutton.classList.add("fa-solid");
+    deletebutton.classList.add("fa-trash");
+    deletebutton.classList.add("delete");
+    currentSaving.appendChild(deletebutton);
+    allSavingDiv.appendChild(currentSaving);
+})
+//delete
+const deleteButtons=document.querySelectorAll(".delete");
+const modal=document.querySelector(".delete_modal")
+deleteButtons.forEach(function(delbtn){
+    delbtn.addEventListener("click",function(){
+        modal.style.display="flex";
+        const savingDiv=delbtn.parentElement;
+        window.savingToDelete = savingDiv.dataset.id;
+
+    })
+})
+const deleteYes=document.querySelector("#delete");
+const deleteNo=document.querySelector("#dont-delete");
+deleteNo.addEventListener("click",function(){
+    modal.style.display="none";
+})
+deleteYes.addEventListener("click", function () {
+    user.savingsHistory = user.savingsHistory.filter(s => s.id != window.savingToDelete);
+    localStorage.setItem("users", JSON.stringify(userList));
+    location.reload();
+
+});
+
+
+//SEARCH BAR
+const searchBar=document.querySelector("#search_savings");
+searchBar.addEventListener("input",function(){
+    const text=searchBar.value.toLowerCase();
+    const savingsDivs=document.querySelectorAll(".savings");
+    savingsDivs.forEach(function(div,index){
+        const description = div.querySelector(".savings_details_name").innerText.toLowerCase();
+        if(description.includes(text)){
+            div.style.display="flex";
+        }
+        else{
+            div.style.display="none";
+        }
+    })
+})
